adb92a6fc32639fd51e218cdf170920b8a58447b
Nick Satterly <nick.satterly@guardian.co.uk>
2014-03-24 22:11:43 +0000

    move up one level

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 
 import setuptools
-from alerta.client import __version__
+from alertacli import __version__
 
 with open('README.rst') as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="alerta-api",
+    name="alertacli",
     version=__version__,
-    description="Alerta client API for python",
+    description="Alerta unified command-line tool",
     long_description=long_description,
     license="MIT",
     author="Nick Satterly",
     author_email="nick.satterly@theguardian.com",
-    url="http://github.com/alerta/python-alertaclient",
+    url="http://github.com/alerta/alertacli",
     packages=setuptools.find_packages(exclude=['tests']),
     install_requires=[
         'requests'
     ],
-    keywords="alerta library",
+    keywords="alerta command line",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
@@ -27,6 +27,5 @@ setuptools.setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: System :: Monitoring',
-    ],
-    zip_safe=True
+    ]
 )
