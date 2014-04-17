2586cc8c7bcd9a22fa219c798863293dab387fa9
Nick Satterly <nick.satterly@guardian.co.uk>
2014-04-17 09:50:40 +0100

    bump version to 3.0.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
 
 setuptools.setup(
     name="alerta-client",
-    version='3.0.5',
+    version='3.0.7',
     description="Alerta unified command-line tool",
     long_description=long_description,
     license="MIT",
