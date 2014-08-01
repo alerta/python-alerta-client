6e024501b76beaccc2daa46f045f4e3444aa146b
Nick Satterly <nick.satterly@guardian.co.uk>
2014-08-01 17:18:32 +0100

    update python client library example

--- examples/send.py
+++ examples/send.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 
-from alerta.client import Alert, ApiClient
+from alerta.api import ApiClient
+from alerta.alert import Alert
 
-client = ApiClient()
+api = ApiClient(endpoint='http://localhost:8080', key='tUA6oBX6E5hUUQZ+dyze6vZbOMmiZWA7ke88Nvio')
 
-alert = Alert(resource='res1', event='event1')
+alert = Alert(
+    resource='web-server-01',
+    event='HttpError',
+    group='Web',
+    environment='Production',
+    service='theguardian.com',
+    severity='major',
+    value='Bad Gateway (502)',
+    text='Web server error.',
+    tags=['web', 'dc1', 'london'],
+    attributes={'customer': 'The Guardian'}
+)
 print alert
 
-print client.send(alert)
+try:
+    print api.send(alert)
+except Exception as e:
+    print e
