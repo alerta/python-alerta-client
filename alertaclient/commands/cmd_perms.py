c179c19e1d2c52a918149e05578f1f89d8314fb6
Nick Satterly <nfsatterly@gmail.com>
2018-11-21 17:39:16 +0100

    Allow filter users and roles for RBAC

--- alertaclient/commands/cmd_perms.py
+++ alertaclient/commands/cmd_perms.py
@@ -6,14 +6,16 @@ from tabulate import tabulate
 
 
 @click.command('perms', short_help='List role-permission lookups')
+@click.option('--scope', 'scopes', multiple=True, help='Filter roles by scope eg. admin:keys, write:alerts')
 @click.pass_obj
-def cli(obj):
+def cli(obj, scopes):
     """List permissions."""
     client = obj['client']
+    query = [('scopes', s) for s in scopes]
 
     if obj['output'] == 'json':
-        r = client.http.get('/perms')
+        r = client.http.get('/perms', query)
         click.echo(json.dumps(r['permissions'], sort_keys=True, indent=4, ensure_ascii=False))
     else:
         headers = {'id': 'ID', 'scopes': 'SCOPES', 'match': 'ROLE'}
-        click.echo(tabulate([p.tabular() for p in client.get_perms()], headers=headers, tablefmt=obj['output']))
+        click.echo(tabulate([p.tabular() for p in client.get_perms(query)], headers=headers, tablefmt=obj['output']))
