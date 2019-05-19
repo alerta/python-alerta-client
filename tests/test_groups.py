73006466a9e36b9e972fd9890bf86b8c48c7526e
Nick Satterly <nfsatterly@gmail.com>
2019-05-19 20:30:21 +0200

    Update client API interface with groups and notes (#184)

---
+++ tests/test_groups.py
@@ -0,0 +1,33 @@
+
+import unittest
+
+import requests_mock
+
+from alertaclient.api import Client
+
+
+class GroupTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+        self.key = """
+            {
+                "group": {
+                    "count": 0,
+                    "href": "http://localhost:8080/group/8ed5d256-4205-4dfc-b25d-185bd019cb21",
+                    "id": "8ed5d256-4205-4dfc-b25d-185bd019cb21",
+                    "name": "myGroup",
+                    "text": "test group"
+                },
+                "id": "8ed5d256-4205-4dfc-b25d-185bd019cb21",
+                "status": "ok"
+            }
+        """
+
+    @requests_mock.mock()
+    def test_key(self, m):
+        m.post('http://localhost:8080/group', text=self.key)
+        group = self.client.create_group(name='myGroup', text='test group')
+        self.assertEqual(group.name, 'myGroup')
+        self.assertEqual(group.text, 'test group')
---
+++ tests/test_notes.py
@@ -0,0 +1,24 @@
+
+import unittest
+
+import requests_mock
+
+from alertaclient.api import Client
+
+
+class NotesTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+        self.key = """
+            {
+              "status": "ok"
+            }
+        """
+
+    @requests_mock.mock()
+    def test_add_note(self, m):
+        m.put('http://localhost:8080/alert/e7020428-5dad-4a41-9bfe-78e9d55cda06/note', text=self.key)
+        r = self.client.add_note(id='e7020428-5dad-4a41-9bfe-78e9d55cda06', note='this is a test note')
+        self.assertEqual(r['status'], 'ok')
