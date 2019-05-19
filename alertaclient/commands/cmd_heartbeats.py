c567c8bf5db96b06757cf09427cefc247699669a
Nick Satterly <nfsatterly@gmail.com>
2019-05-19 22:19:50 +0200

    Send heartbeat alerts if json format used

--- alertaclient/commands/cmd_heartbeats.py
+++ alertaclient/commands/cmd_heartbeats.py
@@ -4,7 +4,7 @@ import json
 import click
 from tabulate import tabulate
 
-from alertaclient.models.heartbeat import MAX_LATENCY
+from alertaclient.models.heartbeat import MAX_LATENCY, Heartbeat
 
 
 @click.command('heartbeats', short_help='List heartbeats')
@@ -19,6 +19,7 @@ def cli(obj, alert, severity, timeout, purge):
 
     if obj['output'] == 'json':
         r = client.http.get('/heartbeats')
+        heartbeats = [Heartbeat.parse(hb) for hb in r['heartbeats']]
         click.echo(json.dumps(r['heartbeats'], sort_keys=True, indent=4, ensure_ascii=False))
     else:
         timezone = obj['timezone']
@@ -29,65 +30,65 @@ def cli(obj, alert, severity, timeout, purge):
         heartbeats = client.get_heartbeats()
         click.echo(tabulate([h.tabular(timezone) for h in heartbeats], headers=headers, tablefmt=obj['output']))
 
-        not_ok = [hb for hb in heartbeats if hb.status != 'ok']
-        if purge:
-            with click.progressbar(not_ok, label='Purging {} heartbeats'.format(len(not_ok))) as bar:
-                for b in bar:
-                    client.delete_heartbeat(b.id)
+    not_ok = [hb for hb in heartbeats if hb.status != 'ok']
+    if purge:
+        with click.progressbar(not_ok, label='Purging {} heartbeats'.format(len(not_ok))) as bar:
+            for b in bar:
+                client.delete_heartbeat(b.id)
 
-        elif alert:
-            with click.progressbar(heartbeats, label='Alerting {} heartbeats'.format(len(heartbeats))) as bar:
-                for b in bar:
-                    params = dict(filter(lambda a: len(a) == 2, map(lambda a: a.split(':'), b.tags)))
-                    environment = params.get('environment', 'Production')
-                    group = params.get('group', 'System')
-                    tags = list(filter(lambda a: not a.startswith('environment:') and
-                                       not a.startswith('group:'), b.tags))
+    if alert:
+        with click.progressbar(heartbeats, label='Alerting {} heartbeats'.format(len(heartbeats))) as bar:
+            for b in bar:
+                params = dict(filter(lambda a: len(a) == 2, map(lambda a: a.split(':'), b.tags)))
+                environment = params.get('environment', 'Production')
+                group = params.get('group', 'System')
+                tags = list(filter(lambda a: not a.startswith('environment:') and
+                                   not a.startswith('group:'), b.tags))
 
-                    if b.status == 'expired':  # aka. "stale"
-                        client.send_alert(
-                            resource=b.origin,
-                            event='HeartbeatFail',
-                            correlate=['HeartbeatFail', 'HeartbeatSlow', 'HeartbeatOK'],
-                            group=group,
-                            environment=environment,
-                            service=['Alerta'],
-                            severity=severity,
-                            value='{}'.format(b.since),
-                            text='Heartbeat not received in {} seconds'.format(b.timeout),
-                            tags=tags,
-                            type='heartbeatAlert',
-                            timeout=timeout,
-                            customer=b.customer
-                        )
-                    elif b.status == 'slow':
-                        client.send_alert(
-                            resource=b.origin,
-                            event='HeartbeatSlow',
-                            correlate=['HeartbeatFail', 'HeartbeatSlow', 'HeartbeatOK'],
-                            group=group,
-                            environment=environment,
-                            service=['Alerta'],
-                            severity=severity,
-                            value='{}ms'.format(b.latency),
-                            text='Heartbeat took more than {}ms to be processed'.format(MAX_LATENCY),
-                            tags=tags,
-                            type='heartbeatAlert',
-                            timeout=timeout,
-                            customer=b.customer
-                        )
-                    else:
-                        client.send_alert(
-                            resource=b.origin,
-                            event='HeartbeatOK',
-                            correlate=['HeartbeatFail', 'HeartbeatSlow', 'HeartbeatOK'],
-                            group=group,
-                            environment=environment,
-                            service=['Alerta'],
-                            severity='normal',
-                            value='',
-                            text='Heartbeat OK',
-                            tags=tags,
-                            type='heartbeatAlert',
-                            customer=b.customer
-                        )
+                if b.status == 'expired':  # aka. "stale"
+                    client.send_alert(
+                        resource=b.origin,
+                        event='HeartbeatFail',
+                        correlate=['HeartbeatFail', 'HeartbeatSlow', 'HeartbeatOK'],
+                        group=group,
+                        environment=environment,
+                        service=['Alerta'],
+                        severity=severity,
+                        value='{}'.format(b.since),
+                        text='Heartbeat not received in {} seconds'.format(b.timeout),
+                        tags=tags,
+                        type='heartbeatAlert',
+                        timeout=timeout,
+                        customer=b.customer
+                    )
+                elif b.status == 'slow':
+                    client.send_alert(
+                        resource=b.origin,
+                        event='HeartbeatSlow',
+                        correlate=['HeartbeatFail', 'HeartbeatSlow', 'HeartbeatOK'],
+                        group=group,
+                        environment=environment,
+                        service=['Alerta'],
+                        severity=severity,
+                        value='{}ms'.format(b.latency),
+                        text='Heartbeat took more than {}ms to be processed'.format(MAX_LATENCY),
+                        tags=tags,
+                        type='heartbeatAlert',
+                        timeout=timeout,
+                        customer=b.customer
+                    )
+                else:
+                    client.send_alert(
+                        resource=b.origin,
+                        event='HeartbeatOK',
+                        correlate=['HeartbeatFail', 'HeartbeatSlow', 'HeartbeatOK'],
+                        group=group,
+                        environment=environment,
+                        service=['Alerta'],
+                        severity='normal',
+                        value='',
+                        text='Heartbeat OK',
+                        tags=tags,
+                        type='heartbeatAlert',
+                        customer=b.customer
+                    )
