f745ce828a0949b63b4e83e13ac8106273d0a162
Nick Satterly <nfsatterly@gmail.com>
2019-05-20 15:43:55 +0200

    Use markdown as README format

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 
+import os
+
 import setuptools
 
-with open('VERSION') as f:
-    version = f.read().strip()
 
-with open('README.md') as f:
-    readme = f.read()
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
 
 setuptools.setup(
     name='alerta',
-    version=version,
+    version=read('VERSION'),
     description='Alerta unified command-line tool and SDK',
-    long_description=readme,
-    url='http://github.com/alerta/python-alerta',
-    license='MIT',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    license='Apache License 2.0',
     author='Nick Satterly',
-    author_email='nick.satterly@theguardian.com',
+    author_email='nick.satterly@gmail.com',
     packages=setuptools.find_packages(exclude=['tests']),
     install_requires=[
         'Click',
@@ -35,13 +36,15 @@ setuptools.setup(
     keywords='alerta client unified command line tool sdk',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
         'Intended Audience :: Information Technology',
         'Intended Audience :: System Administrators',
         'Intended Audience :: Telecommunications Industry',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.5',
         'Topic :: System :: Monitoring',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     python_requires='>=3.5'
 )
