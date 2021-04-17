eda07569d26d94a5e5db47b3eeac8774a447116c
Nick Satterly <nfsatterly@gmail.com>
2021-04-17 23:27:52 +0200

    feat: add option to show decoded auth token claims (#254)

--- alertaclient/commands/cmd_token.py
+++ alertaclient/commands/cmd_token.py
@@ -1,11 +1,21 @@
 import click
 
+from alertaclient.auth.token import Jwt
 from alertaclient.auth.utils import get_token
 
 
 @click.command('token', short_help='Display current auth token')
+@click.option('--decode', '-D', is_flag=True, help='Decode auth token.')
 @click.pass_obj
-def cli(obj):
-    """Display the auth token for logged in user."""
+def cli(obj, decode):
+    """Display the auth token for logged in user, with option to decode it."""
     client = obj['client']
-    click.echo(get_token(client.endpoint))
+    token = get_token(client.endpoint)
+    if decode:
+        jwt = Jwt()
+        for k, v in jwt.parse(token).items():
+            if isinstance(v, list):
+                v = ', '.join(v)
+            click.echo('{:20}: {}'.format(k, v))
+    else:
+        click.echo(token)
