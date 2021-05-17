32ea389caf3fb236e5b2e95b46f6a799dd6a64b4
Matthieu Serrepuy <matthieu@serrepuy.fr>
2021-05-17 22:26:15 +0200

    Add a --alert flag to alert keys to alert on expired and expiring key

--- alertaclient/commands/cmd_keys.py
+++ alertaclient/commands/cmd_keys.py
@@ -1,12 +1,19 @@
 import json
+from datetime import datetime, timedelta
 
 import click
 from tabulate import tabulate
 
+from alertaclient.utils import origin
+
 
 @click.command('keys', short_help='List API keys')
+@click.option('--alert', is_flag=True, help='Alert on expiring and expired keys')
+@click.option('--maxage', metavar='DAYS', default=7, type=int, help='Max remaining days before alerting')
+@click.option('--timeout', metavar='SECONDS', default=86400, type=int, help='Seconds before expired key alerts will be expired')
+@click.option('--severity', '-s', metavar='SEVERITY', default='warning', help='Severity for expiring and expired alerts')
 @click.pass_obj
-def cli(obj):
+def cli(obj, alert, maxage, severity, timeout):
     """List API keys."""
     client = obj['client']
 
@@ -20,3 +27,62 @@ def cli(obj):
             'expireTime': 'EXPIRES', 'count': 'COUNT', 'lastUsedTime': 'LAST USED', 'customer': 'CUSTOMER'
         }
         click.echo(tabulate([k.tabular(timezone) for k in client.get_keys()], headers=headers, tablefmt=obj['output']))
+
+    if alert:
+        keys = r['keys']
+        service = ['Alerta']
+        group = 'System'
+        environment = 'Production'
+        with click.progressbar(keys, label=f'Analysing {len(keys)} keys') as bar:
+            for b in bar:
+                if b['status'] == 'expired':
+                    client.send_alert(
+                        resource=b['id'],
+                        event='ApiKeyExpired',
+                        environment=environment,
+                        severity=severity,
+                        correlate=['ApiKeyExpired', 'ApiKeyExpiring', 'ApiKeyOK'],
+                        service=service,
+                        group=group,
+                        value='Expired',
+                        text=f"Key expired on {b['expireTime']}",
+                        origin=origin(),
+                        type='apiKeyAlert',
+                        timeout=timeout,
+                        customer=b['customer']
+                    )
+                elif b['status'] == 'active':
+                    expiration = datetime.fromisoformat(b['expireTime'].split('.')[0])
+                    remaining_validity = expiration - datetime.now()
+                    if remaining_validity < timedelta(days=maxage):
+                        client.send_alert(
+                            resource=b['id'],
+                            event='ApiKeyExpiring',
+                            environment=environment,
+                            severity=severity,
+                            correlate=['ApiKeyExpired', 'ApiKeyExpiring', 'ApiKeyOK'],
+                            service=service,
+                            group=group,
+                            value=str(remaining_validity),
+                            text=f"Key is active and expires on {b['expireTime']}",
+                            origin=origin(),
+                            type='apiKeyAlert',
+                            timeout=timeout,
+                            customer=b['customer']
+                        )
+                    else:
+                        client.send_alert(
+                            resource=b['id'],
+                            event='ApiKeyOK',
+                            environment=environment,
+                            severity='ok',
+                            correlate=['ApiKeyExpired', 'ApiKeyExpiring', 'ApiKeyOK'],
+                            service=service,
+                            group=group,
+                            value=str(remaining_validity),
+                            text=f"Key is active and expires on {b['expireTime']}",
+                            origin=origin(),
+                            type='apiKeyAlert',
+                            timeout=timeout,
+                            customer=b['customer']
+                        )
