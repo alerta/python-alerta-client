c24a8314f54e641114a9e1e35b42c1304d69c597
Nick Satterly <nfsatterly@gmail.com>
2021-12-05 23:07:56 +0100

    Release 8.7 RC1

--- setup.py
+++ setup.py
@@ -45,9 +45,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.6',
         'Topic :: System :: Monitoring',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    python_requires='>=3.6'
+    python_requires='>=3.7'
 )
